b4878beb91aee4274bd68fd1982b1a3eeab7d444
Eluifree4all <108718314+Eluifree4all@users.noreply.github.com>
2024-09-22 21:15:43 +0100

    Add files via upload
    main files

---
+++ index.js
@@ -0,0 +1,50 @@
+let hours = 0;
+let minutes = 0;
+var seconds = 0;
+let timerCount;
+let time;
+
+let setTime = () => {
+  hours < 10 ? (hoursDisplay = "0" + hours) : (hoursDisplay = hours);
+  minutes < 10 ? (minutesDisplay = "0" + minutes) : (minutesDisplay = minutes);
+  seconds < 10 ? (secondsDisplay = "0" + seconds) : (secondsDisplay = seconds);
+
+  time = "Time:  " + hoursDisplay + ":" + minutesDisplay + ":" + secondsDisplay;
+  document.getElementById("timer").innerText = time;
+};
+
+let timer = () => {
+  if (seconds < 60) {
+    seconds = seconds + 1;
+  } else if (minutes < 60) {
+    minutes = minutes + 1;
+    seconds = 0;
+  } else {
+    hours = hours + 1;
+    minutes = 0;
+    seconds = 0;
+  }
+  setTime();
+};
+
+let stop = () => {
+  if (timerCount) {
+    console.log("working");
+    clearInterval(timerCount);
+    timerCount = null;
+  }
+};
+
+let reset = () => {
+  console.log("yesha");
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  setTime();
+};
+
+let start = () => {
+  if (!timerCount) {
+    timerCount = setInterval(timer, 1000);
+  }
+};
